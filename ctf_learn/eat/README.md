677a5636ff03fab9df41885bf189836b21b5fc2d
minhkhanh091 <minhkhanh220706@gmail.com>
2025-05-07 10:38:15 +0700

    add: new challenge eat

---
+++ ctf_learn/eat/README.md
@@ -0,0 +1,35 @@
+# eat
+
+## 🧩 Challenge Info
+
+- **Category**: reverse
+- **Attachment(s)**: `eat.py`
+
+---
+
+## 🧠 Analysis
+
+Describe how you analyzed the challenge. What files/tools did you look at?  
+Any interesting clues? What made you suspicious?
+
+- Tool(s) used: 
+- Initial observations:
+- ...
+- Approach:
+- ...
+
+---
+
+## 🔍 Solution
+
+Explain how you solved the challenge step by step. Include code, commands, screenshots (if needed), and your reasoning.
+
+### Step 1: ...
+Explain what you did and why.
+
+### Step 2: ...
+Repeat for each logical part of the solution.
+
+---
+
+## 🔑 Flag: `341eat009`
---
+++ ctf_learn/eat/eat.py
@@ -0,0 +1,62 @@
+# I wrote and debugged this code with all the convoluted "int" variable names.
+# Was it confusing? Yes. Was debugging hard? Yes.
+# Did I spend more time than I should have on this problem? Yes
+
+EATEATEATEATEATEAT = str.isdigit
+
+def EAt(eat, eats): # #E10#a23#t90#90t#9ae#014#0 -> eats = Eat9900 | eat = 1023900tae14
+    print(eat)
+    print(eats)
+    eat1 = 0
+    eat2 = 0
+    eateat = 0
+    res = ""
+    while eat1 < len(eat) and eat2 < len(eats):
+        if eateat % 3 == 2 // 4:
+            res += eats[eat2]
+            eat2 += 1
+        else:
+            res += eat[eat1]
+            eat1 += 1
+        eateat += 1
+    return res
+
+def Eating(eat):
+    return str((int(eat) * 3))
+
+# 900______
+def eaT(eat):
+    return Eating(eat[:3]) + eat[::-1] # 900______
+
+def Ate(eat):
+    return "Eat" + "9" + eat[:3]
+
+def Eat(eat):
+    if len(eat) == 9:
+        if EATEATEATEATEATEAT(eat[:3]) and EATEATEATEATEATEAT(eat[7:]):
+                # eaT(eat) = 1023900tae14 -> 341 -> 900tae143
+                # Ate(eat[::-1]) = Eat9900
+                eateat = EAt(eaT(eat), Ate(eat[::-1])) 
+                if eateat == "E10a23t9090t9ae0140":
+                    flag = "eaten_" + eat
+                    print("absolutely EATEN!!! CTFlearn{",flag,"}")
+                else:
+                    print("thats not the answer. you formatted it fine tho, here's what you got\n>>", eateat)
+        else:
+            print("thats not the answer. bad format :(\
+            \n(hint: 123abc456 is an example of good format)")
+    else:
+        print("thats not the answer. bad length :(")
+
+print("what's the answer")
+eat = input()
+Eat(eat)
+
+
+# Prove for flag:
+# - first 3 char must be number!
+# - 
+
+# flag: XXX___009
+
+# 900______
